8aef82e5bc317a945c47a19788cb646365ad9219
xilwen <v72807647@gmail.com>
2015-05-11 23:36:03 +0800

    BMPLoader 功能完成
    待測試和調整完成後將移除DEBUG INFO。

--- ElemetryScholarMathematics/BMPloader.cpp
+++ ElemetryScholarMathematics/BMPloader.cpp
@@ -1,1 +1,65 @@
 #include "BMPloader.h"
+#include "ConvexHull.h"
+
+
+BMPloader::BMPloader(std::string fileLocation)
+{
+	std::fstream in;
+	in.open(fileLocation);
+	if (!in.is_open())
+	{
+#if _DEBUG
+		std::cout << "No file readed!" << std::endl;
+		getchar();
+#endif
+		exit(EXIT_FAILURE);// handler needed
+	}
+	in >> std::noskipws;
+	unsigned char tmp;
+	while (in >> tmp)
+	{
+		fileStorage.push_back(tmp);
+	}
+	file.close();
+	findDots();
+}
+
+BMPloader::~BMPloader()
+{
+	
+}
+
+void BMPloader::findDots()
+{
+	char temp;
+	unsigned int row = 0, column = 0;
+	for (int i = 62; i < fileStorage.size(); i++)
+	{
+		temp = fileStorage[i];
+		for (int j = 0; j < 8; j++)
+		{
+			row++;
+
+			if (temp % 2 == 0)
+			{
+#if _DEBUG
+				std::cout << "(" << row << "," << column << ") has a dot!" << std::endl;
+#endif
+				x.push_back(row);
+				y.push_back(column);
+			}
+			temp = temp >> 1;
+			if (row == 512)
+			{
+				column++;
+				row = 0;
+			}
+		}
+	}
+	
+}
+
+void BMPloader::toConvexHull()
+{
+	ConvexHull convexhull(x, y);
+}
--- ElemetryScholarMathematics/BMPloader.h
+++ ElemetryScholarMathematics/BMPloader.h
@@ -3,13 +3,26 @@
 #ifndef BMPLOADER_H
 #define BMPLOADER_H
 
+#include <iostream>
+#include <fstream>
+#include <vector>
+
 class BMPloader
 {
 public:
 	BMPloader() = delete;
+	//開檔案
+	BMPloader(std::string);
+	void findDots();
+	void toConvexHull();
 
+	//關檔案
+	~BMPloader();
 private:
-
+	std::fstream file;
+	std::vector<char> fileStorage;
+	std::vector<short> x;
+	std::vector<short> y;
 };
 
 #endif
