1b68adb7790bf4307d331d1505d3ed280ea4e752
qqwerfdsa930 <qqwerfdsa930@gmail.com>
2015-06-16 08:35:27 +0800

    煥熊圖+鑰匙

--- ElemetryScholarMathematics/Script.cpp
+++ ElemetryScholarMathematics/Script.cpp
@@ -24,30 +24,31 @@ void Script::runScripts()
 	ui->fullScreenDialog("..");
 
 
-	ui->showDialog("謎之音", "你終於來了。", 0, true, 1, false);
-	ui->showDialog("謎之音", "我等你很久了ㄏㄏ", 0, true, 1, false);
+	ui->showDialog("謎之音", "你終於來了。", 0, true, 4, false, true);
+	ui->showDialog("謎之音", "我等你很久了ㄏㄏ", 0, true, 4, false, true);
 
 	/*開始命名*/
 	ui->chooseNamae();
-	ui->showDialog("謎之音", "哼哈哈哈，叫做" + ui->getName() + "是嗎。真是個好名字。", 0, true, 1, false);
+	ui->showDialog("謎之音", "哼哈哈哈，叫做" + ui->getName() + "是嗎。真是個好名字。", 0, true, 4, false, true);
+	ui->showDialog(ui->getName(), "...莫名其妙", 0);
 
 	/*開門動畫*/
-	ui->showDialog(ui->getName(), "是你阿，凌娜。", 0, true, 1, false);
-	ui->showDialog("凌娜", "...有找到什麼嗎？", 0, true, 1, false);
-	ui->showDialog(ui->getName(), "有...但是他們在一個恐怖的世界裡。", 0, true, 1, false);
-	ui->showDialog("凌娜", "恐怖的世界裡？你要編故事也該正常點！", "你根本就是有病吧！", 0, true, 1, false);
-	ui->showDialog(ui->getName(), "你說什麼！我說的都是真的阿！", 0, true, 1, false);
-	ui->showDialog("凌娜", "那好 ...", 0, true, 1, false);
+	ui->showDialog(ui->getName(), "是你阿，凌娜。", 0, true, 1, false, false);
+	ui->showDialog("凌娜", "...有找到什麼嗎？", 0, true, 1, false, true);
+	ui->showDialog(ui->getName(), "有...但是他們在一個恐怖的世界裡。", 0, true, 1, false, false);
+	ui->showDialog("凌娜", "恐怖的世界裡？你要編故事也該正常點！", "你根本就是有病吧！", 0, true, 1, false, true);
+	ui->showDialog(ui->getName(), "你說什麼！我說的都是真的阿！", 0, true, 1, false, false);
+	ui->showDialog("凌娜", "那好 ...", 0, true, 1, false, true);
 
 	/*掏鑰匙*/
-	ui->showDialog("凌娜", "你答對我的問題我就承認你說的是真的。", 0, true, 1, false);
+	ui->showDialog("凌娜", "你答對我的問題我就承認你說的是真的。", 1);
 
 	/*I part*/
 	int a, b, answer;
 	a = (rand() % 10 + 1);
 	b = (rand() % 10 + 1);
 	ui->showDialog("凌娜", "我左手上有" + std::to_string(a) + "把鑰匙，我右手上有"
-						+ std::to_string(b) + "把鑰匙，請問我有幾把鑰匙？", 0, true, 1, false);
+						+ std::to_string(b) + "把鑰匙，請問我有幾把鑰匙？", 11, true, 1, false);
 	
 	//加個凌娜鑰匙產生器吧
 
@@ -55,48 +56,41 @@ void Script::runScripts()
 	answer = ui->userChoice(ui->getName(), std::to_string(a + b), std::to_string(a*b), std::to_string(a - b), std::to_string(b-a), 0);
 	if (answer == 0)
 	{
-		ui->showDialog("凌娜", "哼！", 0, true, 1, false);
-		/*穿過user身上*/
-		ui->showDialog(ui->getName(), "......", 0, true, 1, false);
+		ui->showDialog("凌娜", "哼！", 0, true, 1, false, true);
 	}
 	else
 	{
-		ui->showDialog("凌娜", "你還是吃屎吧！" + ui->getName() + "！答案是 " + std::to_string(a + b), 0, true, 1, false);
-		/*if (ui->getName() == "龍傲天")
-		{
-			ui->showDialog("交趾國東征先鋒大將", "想取代戰神，先學會仰望我！");
-			ui->dontB();
-		}
-		else
-		{
-			ui->showDialog("交趾國東征先鋒大將", "順我者生，逆我者亡!哇哈哈~~");
-			ui->dead();
-		}*/
+		ui->showDialog("凌娜", "你還是吃屎吧！" + ui->getName() + "！答案是 " + std::to_string(a + b), 0, true, 1, false, true);
 	}
+	
+	/*穿過user身上*/
+	ui->showDialog(ui->getName(), "......", 0, true, 1, false, false);
 
 	/*II part*/
 
-	ui->showDialog("魯凱", "...", 0, true, 3, false);
-	ui->showDialog("魯凱", ui->getName() + "，我一直想要住在這種雙人小套房。", 0, true, 3, false);
+	ui->showDialog("魯凱", "...", 0, true, 2, false, true);
+	ui->showDialog("魯凱", ui->getName() + "，我一直想要住在這種雙人小套房。", 0, true, 2, false, true);
 	
 	/*換表情(驚嚇)*/
-	ui->showDialog(ui->getName(), "什麼...", 0, true, 3, false);
-	ui->showDialog("魯凱", "可以跟我心愛的人住在一起這樣多棒。", 0, true, 3, false);
-	ui->showDialog(ui->getName(), "......對了，", "這個東西好特別喔！", 0, true, 3, false);
-	ui->showDialog("魯凱", "什麼東西？", 0, true, 3, false);
+	ui->showDialog(ui->getName(), "什麼...", 0, true, 2, false, false);
+	ui->showDialog("魯凱", "可以跟我心愛的人住在一起這樣多棒。", 0, true, 6, false, true);
+	ui->showDialog(ui->getName(), "(跟我說這些要幹嘛阿！！", "難道...)", 0);
+	ui->showDialog(ui->getName(), "不行！想辦法轉移他的注意力。", 0);
+	ui->showDialog(ui->getName(), "......對了，", "這個東西好特別喔！", 0, true, 2, false, false);
+	ui->showDialog("魯凱", "什麼東西？", 0, true, 2, false, true);
 	
 	int choice;
 
 	choice = ui->userChoice(ui->getName(), "床", "三角胖次", "平行四邊形地毯", "春聯", 0);
-	ui->showDialog("魯凱", "怎麼了嗎？", 0, true, 3, false);
+	ui->showDialog("魯凱", "怎麼了嗎？", 0, true, 2, false, false);
 
 
 	if (choice == 0)
 	{
-		ui->showDialog(ui->getName(), "好...好大阿！？", 0, true, 3, false);
+		ui->showDialog(ui->getName(), "好...好大阿！？", 0, true, 2, false, false);
 		/*魯凱表示害羞*/
-		ui->showDialog("魯凱", "......", 0, true, 3, false);
-		ui->showDialog(ui->getName(), "不是啦！這是多少乘多少的床，", "怎麼這麼大？", 0, true, 3, false);
+		ui->showDialog("魯凱", "......", 0, true, 6, false, true);
+		ui->showDialog(ui->getName(), "不是啦！這是多少乘多少的床，", "怎麼這麼大？", 0, true, 2, false, false);
 		//
 		if ((a % 3) == 0)
 		{
@@ -109,7 +103,7 @@ void Script::runScripts()
 			SA.Show_Rectangle(botRec, heiRec);
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
-			ui->showDialog("魯凱", "這是" + std::to_string(botRec) + " * " + std::to_string(heiRec), "面積為" + std::to_string(botRec * heiRec), 0, true, 3, false);
+			ui->showDialog("魯凱", "這是" + std::to_string(botRec) + " * " + std::to_string(heiRec), "面積為" + std::to_string(botRec * heiRec), 0, true, 2, false, true);
 		}
 		else if ((a % 3) == 1)
 		{
@@ -121,7 +115,7 @@ void Script::runScripts()
 			SA.Show_Rectangle(botRec, heiRec);
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
-			ui->showDialog("魯凱", "這是" + std::to_string(botRec) + " * " + std::to_string(heiRec), "面積為" + std::to_string(botRec * heiRec), 0, true, 3, false);
+			ui->showDialog("魯凱", "這是" + std::to_string(botRec) + " * " + std::to_string(heiRec), "面積為" + std::to_string(botRec * heiRec), 0, true, 2, false, true);
 		}
 		else if ((a % 3) == 2)
 		{
@@ -133,15 +127,15 @@ void Script::runScripts()
 			SA.Show_Rectangle(botRec, heiRec);
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
-			ui->showDialog("魯凱", "這是" + std::to_string(botRec) + " * " + std::to_string(heiRec), "面積為" + std::to_string(botRec * heiRec), 0, true, 3, false);	
+			ui->showDialog("魯凱", "這是" + std::to_string(botRec) + " * " + std::to_string(heiRec), "面積為" + std::to_string(botRec * heiRec), 0, true, 2, false, true);	
 		}
 	}
 	else if (choice == 1)
 	{
-		ui->showDialog(ui->getName(), "好...好特別的...造型！？", 0, true, 3, false);
+		ui->showDialog(ui->getName(), "好...好特別的...造型！？", 0, true, 2, false, false);
 		/*魯凱表示害羞*/
-		ui->showDialog("魯凱", "......", 0, true, 3, false);
-		ui->showDialog(ui->getName(), "不是啦！這...好穿嗎！？", 0, true, 3, false);
+		ui->showDialog("魯凱", "......", 0, true, 6, false, true);
+		ui->showDialog(ui->getName(), "不是啦！這...好穿嗎！？", 0, true, 2, false, false);
 		if ((a % 4) == 0)
 		{
 			int botTri = 18, heiTri = 5;
@@ -153,7 +147,7 @@ void Script::runScripts()
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
 			ui->showDialog("魯凱", "這是褲頭 " + std::to_string(botTri) + " ，長 " + std::to_string(heiTri) + " 的三角形",
-				"面積為" + std::to_string((botTri * heiTri)/2), 0, true, 3, false);
+				"面積為" + std::to_string((botTri * heiTri)/2), 0, true, 2, false, true);
 		}
 		else if ((a % 4) == 1)
 		{
@@ -166,7 +160,7 @@ void Script::runScripts()
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
 			ui->showDialog("魯凱", "這是褲頭 " + std::to_string(botTri) + " ，長 " + std::to_string(heiTri) + " 的三角形",
-				"面積為" + std::to_string((botTri * heiTri) / 2), 0, true, 3, false);
+				"面積為" + std::to_string((botTri * heiTri) / 2), 0, true, 2, false, true);
 		}
 		else if ((a % 4) == 2)
 		{
@@ -179,7 +173,7 @@ void Script::runScripts()
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
 			ui->showDialog("魯凱", "這是褲頭 " + std::to_string(botTri) + " ，長 " + std::to_string(heiTri) + " 的三角形",
-				"面積為" + std::to_string((botTri * heiTri) / 2), 0, true, 3, false);
+				"面積為" + std::to_string((botTri * heiTri) / 2), 0, true, 2, false, true);
 		}
 		else if ((a % 4) == 3)
 		{
@@ -191,89 +185,80 @@ void Script::runScripts()
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
 			ui->showDialog("魯凱", "這是褲頭 " + std::to_string(botTri) + " ，長 " + std::to_string(heiTri) + " 的三角形",
-				"面積為" + std::to_string((botTri * heiTri) / 2), 0, true, 3, false);
+				"面積為" + std::to_string((botTri * heiTri) / 2), 0, true, 2, false, true);
 		}
 	}
 	else if (choice == 3)
 	{
-		ui->showDialog(ui->getName(), "這....阿！", "這裡居然有春聯耶", 0, true, 3, false);
+		ui->showDialog(ui->getName(), "這....阿！", "這裡居然有春聯耶", 0, true, 2, false, false);
 		if (a > 7)
 		{
 			//
+			a = a + 1;
 			ui->clearScreen();
 			ui->gotoxy(0, 2);
 			SA.Show_Diamond(a);
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
 			ui->showDialog("魯凱", "對阿，這是一個 " + std::to_string(a) + " * " + std::to_string(a) + " 的地毯",
-				"面積為 " + std::to_string(a*a), 0, true, 3, false);
+				"面積為 " + std::to_string(a*a), 0, true, 2, false, true);
 		}
 		else
 		{
 			//
+			b = b + 1;
 			ui->clearScreen();
 			ui->gotoxy(0, 2);
 			SA.Show_Diamond(b);
 			ui->print("[ENTER]繼續", 2, 21);
 			getchar();
 			ui->showDialog("魯凱", "對阿，這是一個 " + std::to_string(b) + " * " + std::to_string(b) + " 的地毯",
-				"面積為 " + std::to_string(b*b), 0, true, 3, false);
+				"面積為 " + std::to_string(b*b), 0, true, 2, false, true);
 		}
 	}
 	else if (choice == 2)
 	{
-		ui->showDialog(ui->getName(), "好...好奇特的形狀！？", 0, true, 3, false);
+		ui->showDialog(ui->getName(), "好...好奇特的形狀！？", 0, true, 2, false, false);
 		//
+		a = a + 1;
+		b = b + 1;
 		ui->clearScreen();
 		ui->gotoxy(0, 2);
 		SA.Show_Parallelogram(a, b);;
 		ui->print("[ENTER]繼續", 2, 21);
 		getchar();
 		ui->showDialog("魯凱", "會嗎？就是一個 " + std::to_string(a) + " * " + std::to_string(b) + " 的地毯",
-			"面積為 " + std::to_string(a*b), 0, true, 3, false);
+			"面積為 " + std::to_string(a*b), 0, true, 2, false, true);
 	}
-	//答題
-	//ui->clearScreen();
-	//SimpleArea fromUser;	//給user輸入 mode 及 底邊和高
-	//fromUser.ShowGraph();
-	//fromUser.AreaCalculating();
-	//getchar();
-	//getchar();
 
-	ui->showDialog("魯凱", "你覺得這個尺寸需要換嗎？", 0, true, 3, false);
-	ui->showDialog(ui->getName(), "...現在不是說這些的時候吧？", 0, true, 3, false);
-	ui->showDialog("魯凱", "...是沒錯啦，我只是看到有感而發而已。", "先走了。", 0, true, 3, false);
-
-	
-	
-	//if (ui->getName() == "大中天")
-	//{
-	//	ui->showDialog("凌娜", "你媽怎麼給你取這種名字阿，大中天。", 0, true, 3, false);
-	//}
+	ui->showDialog("魯凱", "你覺得這個尺寸需要換嗎？", 0, true, 2, false, true);
+	ui->showDialog(ui->getName(), "...現在不是說這些的時候吧？", 0, true, 2, false, false);
+	ui->showDialog("魯凱", "...是沒錯啦，我只是看到有感而發而已。", "先走了。", 0, true, 2, false, true);
 
 	/*III part*/
+
 	//黑暗女巫開門動畫
 
-	ui->showDialog("黑暗女巫", "......", 0, true, 4, false);
-	ui->showDialog(ui->getName(), "等等！", 0, true, 4, false);
+	ui->showDialog("黑暗女巫", "......", 0, true, 3, false);
+	ui->showDialog(ui->getName(), "等等！", 0, true, 3, false);
 
 	//追出去
-	ui->showDialog("黑暗女巫", "我只是引誘你，你還真的跟過來了。", 0, true, 4, false);
-	ui->showDialog(ui->getName(), "什麼意思...？", "難道這個詭異的世界是你...？", 0, true, 4, false);
-	ui->showDialog("黑暗女巫", "讓我可愛的寵物陪你玩玩吧...", 0, true, 4, false);
+	ui->showDialog("黑暗女巫", "我只是引誘你，你還真的跟過來了。", 0, true, 3, false);
+	ui->showDialog(ui->getName(), "什麼意思...？", "難道這個詭異的世界是你...？", 0, true, 3, false);
+	ui->showDialog("黑暗女巫", "讓我可愛的寵物陪你玩玩吧...", 0, true, 3, false);
 
 	//跟小寵物打鬧了一下
-	ui->showDialog(ui->getName(), "呼...解決掉他了", 0, true, 4, false);
+	ui->showDialog(ui->getName(), "呼...解決掉他了", 0);
 
 	//石板出場
-	ui->showDialog(ui->getName(), "這是什麼怪東西？", 0, true, 4, false);
+	ui->showDialog(ui->getName(), "這是什麼怪東西？", 0, true, 5, false);
 
-	std::string fileLocation = ui->loadBMP("石板", "找出面積最大的凸多邊形", 0);
+	std::string fileLocation = ui->loadBMP("石板", "找出面積最大的凸多邊形", 5);
 	int rcode = bmpl->init(fileLocation);
 	while (rcode != 0)
 	{
-		ui->showDialog(ui->getName(), "好像是錯覺的樣子。可以不要亂撿地上的東西嗎?(請重開檔案)", 0);
-		std::string fileLocation = ui->loadBMP("石板", "找出面積最大的凸多邊形", 0);
+		ui->showDialog(ui->getName(), "好像是錯覺的樣子。可以不要亂撿地上的東西嗎?(請重開檔案)", 5);
+		std::string fileLocation = ui->loadBMP("石板", "找出面積最大的凸多邊形", 5);
 		rcode = bmpl->init(fileLocation);
 	}
 		
